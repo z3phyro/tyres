1aa282eba2429fbba1245ef556a28c352d1eb719
z3phyro <maykel.meneses89@gmail.com>
2023-08-12 12:35:46 +0200

    Edit dictionary

--- tyres-ui/src/routes/resources/dictionaries/[key]/index.tsx
+++ tyres-ui/src/routes/resources/dictionaries/[key]/index.tsx
@@ -1,5 +1,8 @@
+import { editDictionary } from "@z3phyro/tyres-core";
 import { createEffect, createResource, createSignal } from "solid-js";
-import { useParams, useRouteData } from "solid-start";
+import { useNavigate, useParams, useRouteData } from "solid-start";
+import { createServerAction$ } from "solid-start/server";
+import { ROUTE_PAGE_DICTIONARIES } from "~/config/routes";
 import { EUiVariant } from "~/core/types/ui-variants.type";
 import DictionaryService from "~/services/dictionary.service";
 import Button from "~/stories/components/button";
@@ -7,6 +10,8 @@ import Card from "~/stories/components/card";
 import Input from "~/stories/components/input";
 import Main from "~/stories/components/main";
 import SmartBreadcrumbs from "~/stories/containers/smart-breadcrumbs/smart-breadcrumbs";
+import { useToast } from "~/stories/containers/toast-provider/toast-provider";
+import { ok } from "~/utils/response.helper";
 
 export function routeData() {
   const [dicts] = createResource(async () => {
@@ -20,33 +25,53 @@ export function routeData() {
 export default function Page() {
   const { dicts } = useRouteData<typeof routeData>();
   const { key } = useParams();
+  const toast = useToast();
+  const navigate = useNavigate();
 
-  const [dictKey, setDictKey] = createSignal(key);
+  const [state, { Form }] = createServerAction$(async (formData: FormData) => {
+    if (!formData || !formData.get("key")) return;
+    editDictionary(formData.get("key")!.toString(), formData.get("name")!.toString());
+
+    return ok();
+  });
+
   const [name, setName] = createSignal(dicts()?.[key] ?? "");
 
   createEffect(() => {
     setName(dicts()?.[key] ?? "");
   });
 
+  const result = () => state.result;
+
+  createEffect(() => {
+    if (result()) {
+      toast.success({ title: "Dictionary updated" });
+      navigate(ROUTE_PAGE_DICTIONARIES);
+    }
+  });
+
   return (
     <Main>
       <SmartBreadcrumbs />
-      <Card>
-        <Input
-          label="Key"
-          value={dictKey()}
-          onInput={(e) => setDictKey((e.target as HTMLInputElement).value)}
-        />
-        <Input
-          label="Name"
-          value={name()}
-          onInput={(e) => setName((e.target as HTMLInputElement).value)}
-        />
-      </Card>
-      <div class="flex justify-end gap-2">
-        <Button variant={EUiVariant.Neutral}>Cancel</Button>
-        <Button variant={EUiVariant.Info}>Save</Button>
-      </div>
+      <Form>
+        <Card>
+          <Input label="Key" name="key" value={key} disabled />
+          <Input
+            label="Name"
+            name="name"
+            value={name()}
+            onInput={(e) => setName((e.target as HTMLInputElement).value)}
+          />
+        </Card>
+        <div class="flex justify-end gap-2">
+          <Button variant={EUiVariant.Neutral} onClick={() => navigate(ROUTE_PAGE_DICTIONARIES)}>
+            Cancel
+          </Button>
+          <Button type="submit" variant={EUiVariant.Info} disabled={state.pending}>
+            Save
+          </Button>
+        </div>
+      </Form>
     </Main>
   );
 }
--- tyres-ui/src/stories/components/input/input.tsx
+++ tyres-ui/src/stories/components/input/input.tsx
@@ -3,7 +3,7 @@ import { JSX } from "solid-js";
 
 export interface TInputProps {
   value: string;
-  onInput: (event: Event) => void;
+  onInput?: (event: Event) => void;
   leading?: JSX.Element;
   leadingClass?: string;
   leadingClick?: () => void;
@@ -13,6 +13,8 @@ export interface TInputProps {
   placeholder?: string;
   label?: string;
   hasError?: boolean;
+  name?: string;
+  disabled?: boolean;
 }
 export default function Input(props: TInputProps) {
   return (
@@ -23,10 +25,11 @@ export default function Input(props: TInputProps) {
       <span
         class={`absolute left-3 top-2 transition-color duration-300 ${
           props.value ? props.leadingClass || "text-blue-500" : "text-gray-300"
-        } ${props.leadingClick ? "cursor-pointer" : ""}`}>
+        } ${props.leadingClick ? "cursor-pointer" : ""} `}>
         {props.leading}
       </span>
       <TextField.Input
+        name={props.name ?? props.label}
         class={`w-full p-2 bg-white rounded mb-2 border outline-0 
         focus:shadow-md ${
           props.hasError
@@ -34,10 +37,11 @@ export default function Input(props: TInputProps) {
             : "border-1 focus:border-blue-500 focus:shadow-blue-50"
         } transition-all 
         duration-300 ${props.trailing ? "pr-4" : ""} ${props.leading ? "pl-10" : ""}
-        `}
+        ${props.disabled ? "text-gray-400 cursor-not-allowed" : "bg-white"}`}
         value={props.value}
         onInput={props.onInput}
         placeholder={props.placeholder}
+        disabled={props.disabled}
       />
       <span
         class={`absolute right-3 top-2 transition-color duration-300 ${
