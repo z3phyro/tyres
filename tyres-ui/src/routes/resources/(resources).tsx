f5f5c9b71bee42aa09efaba470b170063816af51
z3phyro <maykel.meneses89@gmail.com>
2024-06-08 17:38:59 +0200

    Use same icons from sidebar in resources index page

--- tyres-ui/src/routes/resources/(resources).tsx
+++ tyres-ui/src/routes/resources/(resources).tsx
@@ -6,9 +6,9 @@ import {
 } from "~/config/routes";
 import { ETerms } from "~/config/terms";
 import Card from "~/stories/components/card";
-import DictionariesIcon from "~/stories/components/icons/dictionaries.icon";
-import FeaturesIcon from "~/stories/components/icons/features.icon";
-import ResourcesIcon from "~/stories/components/icons/resources.icon";
+import FeatureFlagsIcon from "~/stories/components/icons/feature-flags.icon";
+import ShelfIcon from "~/stories/components/icons/shelf.icon";
+import TranslationIcon from "~/stories/components/icons/translation.icon";
 import Main from "~/stories/components/main";
 import SmartBreadcrumbs from "~/stories/containers/smart-breadcrumbs/smart-breadcrumbs";
 
@@ -20,22 +20,22 @@ export default function Page() {
         <A class="flex-1" href={ROUTE_PAGE_I18N}>
           <Card class="flex-1 relative">
             <h1 class="text-xl relative mb-1 z-10">{ETerms.i18n}</h1>
-            <ResourcesIcon class="w-[120px] h-[120px] absolute right-0 bottom-0 z-0 opacity-80" />
-            <p class="mb-[120px] relative z-10">Manage internationalization</p>
+            <TranslationIcon class="w-8 h-8 absolute right-2 bottom-4 z-0 opacity-80" />
+            <p class="relative z-10">Manage internationalization</p>
           </Card>
         </A>
         <A class="flex-1" href={ROUTE_PAGE_DICTIONARIES}>
           <Card class="flex-1 relative">
             <h1 class="text-xl relative mb-1 z-10">{ETerms.Dictionaries}</h1>
-            <DictionariesIcon class="w-[120px] h-[120px] absolute right-2 bottom-2 z-0 opacity-80" />
-            <p class="mb-[120px] relative z-10">Manage dictionaries</p>
+            <ShelfIcon class="w-8 h-8 absolute right-2 bottom-4 z-0 opacity-80" />
+            <p class="relative z-10">Manage dictionaries</p>
           </Card>
         </A>
         <A class="flex-1" href={ROUTE_PAGE_FEATURE_FLAGS}>
           <Card class="flex-1 relative">
             <h1 class="text-xl relative mb-1 z-10">{ETerms.FeatureFlags}</h1>
-            <FeaturesIcon class="w-[120px] h-[120px] absolute right-2 bottom-2 z-0 opacity-80" />
-            <p class="mb-[120px] relative z-10">Manage feature flags</p>
+            <FeatureFlagsIcon class="w-8 h-8 absolute right-4 bottom-2 z-0 opacity-80" />
+            <p class="relative z-10">Manage feature flags</p>
           </Card>
         </A>
       </div>
